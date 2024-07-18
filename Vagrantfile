1ee478aa7e07b08db568caa5ad21c2c12f8088c6
Sayyed Neha Firdous <sayyedneha.firdous@holypoly.co>
2024-07-18 09:27:45 +0200

    changes the curl command and added steps flet_setup.sh

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,11 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-# All Vagrant configuration is done below. The "2" in Vagrant.configure
-# configures the configuration version (we support older styles for
-# backwards compatibility). Please don't change it unless you know what
-# you're doing.
+# TODO: Configure autologin for Ubuntu
+# TODO: Try to start a basic flet gui application in the VM
 Vagrant.configure("2") do |config|
   #config.vm.box = "hashicorp/precise64"
-  config.vm.box = "gusztavvargadr/ubuntu-desktop"
+  config.vm.box = "gusztavvargadr/ubuntu-desktop-2404-lts"
   config.vm.provider "virtualbox" do |vm|  
   vm.gui = true
   config.vm.provision "shell", privileged: false, path: "flet_setup.sh"
   end
 end
+
--- flet_setup.sh
+++ flet_setup.sh
@@ -1,27 +1,33 @@
 #!/bin/bash -e
 
 # Update package lists
-sudo apt update
+sudo apt-get update
 
 # Install prerequisites
-sudo apt install -y python3.11 python3-pip git build-essential
+sudo apt-get install -q -y python3.11 python3-pip git build-essential
 
 # Install Poetry
-if ! which poetry; then
-      curl -sSL https://install.python-poetry.org | python3 -
-fi
+#if ! which poetry; then
+ #     curl -sSL https://install.python-poetry.org | python3 -
+#fi
 
 # Extend PATH to also look at ~/.local/bin where Poetry is located
-if ! grep poetry ~/.profile; then
+#if ! grep poetry ~/.profile; then
+    export PATH="/home/vagrant/.local/bin:$PATH"
+
       echo 'export PATH="~/home/vagrant/.local/bin:$PATH"' >> ~/.profile
-fi 
+#fi
 
 # Use Python 3.11 for the virtual environment
 cd /vagrant
 poetry env use python3.11 
 
+echo step1
+
 # Tools needed for Flutter installation
 which bash file mkdir rm
+
+echo step2
 
 #Installation of Flutter Prerequisites
 sudo apt upgrade -y
@@ -37,16 +43,28 @@ sudo apt install -y \
       libstdc++-12-dev
 
 
+echo step3
+
+
 # TODO: Make this idempotent
 # Download Flutter SDK 
 if [ ! -f "flutter_linux_3.22.2-stable.tar.xz" ]; then
-  curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz
+  wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz 
 fi
-tar -xvJf flutter_linux_3.22.2-stable.tar.xz
+# TODO: make the idempotent, don't extract if the archive is already extracted
+# TODO: Remove all debuging echo lines
+echo step4
+if [ ! -d "flutter" ]; then
+tar -xvJf flutter_linux_3.22.2-stable.tar.xz 
+fi
+
 
 # TODO: Make this idempotent
 # Move Flutter to a permanent location 
 # TODO: Skip moving if flutter directory is already in place at /opt/flutter
+
+echo step5
+
 if [ ! -d "/opt/flutter" ]; then
   sudo mv flutter /opt/flutter
 fi
@@ -54,13 +72,17 @@ fi
 # TODO: make idempotent like extending PATH for poetry, also use .profile
 # Add Flutter to PATH 
 #echo 'export PATH=$PATH:/opt/flutter/bin' >> ~/.bashrc
+echo step6
 
 if ! grep -q ':/opt/flutter/bin' ~/.profile; then
   echo 'export PATH=$PATH:/opt/flutter/bin' >> ~/.profile
 fi
+echo step7
 
 # Source the updated bashrc (optional, but recommended for immediate use)
 source ~/.bashrc
+
+echo step8
 
 # Add Flet dependency to pyproject.toml
 #echo -e "[tool.poetry]\nname = \"flet_project\"\nversion = \"0.1.0\"\ndescription = \"Flet based application\"\n\n[tool.poetry.dependencies]\npython = \"^3.11\"\nflet = \"^0.22.0\" ">> pyproject.toml
@@ -68,3 +90,4 @@ source ~/.bashrc
 # Install dependencies
 poetry install --no-root
 
+echo step9
